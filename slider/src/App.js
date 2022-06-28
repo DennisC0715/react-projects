e9a9841da0de7b8cf09243dd98473de77fe6fff9
Dennis Chen <chensuyuan1989@hotmail.com>
2022-06-28 00:11:08 -0400

    slider

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import data from "./data";
+import { useState, useEffect } from "react";
+import { AiFillLeftSquare, AiFillRightSquare } from "react-icons/ai";
+import { FaQuoteRight } from "react-icons/fa";
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+
+    console.log("running");
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, arrayIndex) => {
+          const { id, image, name, title, quote } = person;
+          let position = "nextSlide";
+          if (arrayIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            arrayIndex === index - 1 ||
+            (index === 0 && arrayIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <div className="title">{title}</div>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon"></FaQuoteRight>
+            </article>
+          );
+        })}
+        <button
+          className="prev"
+          onClick={() => {
+            setIndex(index - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <AiFillLeftSquare />
+        </button>
+        <button
+          className="next"
+          onClick={() => {
+            setIndex(index + 1);
+          }}
+        >
+          <AiFillRightSquare />
+        </button>
+      </div>
+    </section>
   );
 }
 
--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { FaAngleDoubleLeft } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const responseData = await response.json();
+
+    setJobs(responseData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  console.log();
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+  const companies = jobs.map((job) => job.company);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="section">
+        <div className="title">
+          <h2>Experience</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="jobs-center">
+          <div className="btn-container">
+            {companies.map((company, index) => (
+              <button
+                key={index}
+                className={`job-btn ${index === value && "active-btn"}`}
+                onClick={() => {
+                  setValue(index);
+                }}
+              >
+                {company}
+              </button>
+            ))}
+          </div>
+          <div className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty, index) => (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleLeft className="job-icon" /> <p>{duty}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
