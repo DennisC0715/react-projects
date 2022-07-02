d33ec8527eb43faeab465e112a53ee249bcc460d
Dennis Chen <chensuyuan1989@hotmail.com>
2022-07-02 01:47:45 -0400

    finish cart project

--- cart/src/CartContainer.js
+++ cart/src/CartContainer.js
@@ -1,8 +1,36 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { useGlobalContext } from "./context";
 
 const CartContainer = () => {
-  return <div>CartContainer</div>;
+  const { cart, total, clearCart } = useGlobalContext();
+  const isCartEmpty = cart.length === 0;
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>YOUR BAG</h2>
+        {isCartEmpty && <h4 className="empty-cart">is currently empty</h4>}
+      </header>
+      <div>
+        {!isCartEmpty &&
+          cart.map((item) => {
+            return <CartItem item={item} key={item.id} />;
+          })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            Total<span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={clearCart}>
+          CLEAR CART
+        </button>
+      </footer>
+    </section>
+  );
 };
 
 export default CartContainer;
