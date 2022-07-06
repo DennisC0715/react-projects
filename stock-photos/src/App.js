a6d9db9c418ceeb36e51be6c0f8772a5845b4a4c
Dennis Chen <chensuyuan1989@hotmail.com>
2022-07-05 23:48:45 -0400

    quiz  first commit

--- stock-photos/src/App.js
+++ stock-photos/src/App.js
@@ -1,3 +1,18 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+// remove current scroll code
+// set default page to 1
+// setup two useEffects
+// don't run second on initial render
+// check for query value
+// if page 1 fetch images
+// otherwise setPage(1)
+// fix scroll functionality
+
 function App() {
   return;
 }
