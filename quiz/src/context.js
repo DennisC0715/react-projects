13f2381a7428c6d8ecd18258a3c5f67dca1effb7
Dennis Chen <chensuyuan1989@hotmail.com>
2022-07-06 23:59:01 -0400

    setup context

--- quiz/src/context.js
+++ quiz/src/context.js
@@ -1,4 +1,5 @@
 import React, { useContext, createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const table = {
   sports: 21,
@@ -7,9 +8,7 @@ const table = {
 };
 
 const API_ENDPOINT = "https://opentdb.com/api.php?";
-
 const url = "";
-
 const tempUrl =
   "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
@@ -23,17 +22,93 @@ const AppProvider = ({ children }) => {
     difficulty: "easy",
   });
   const [error, setError] = useState();
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [waiting, setWaiting] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
-  const handleChange = () => {};
-  const handleSubmit = () => {};
-
-  const fetchQuiz = async () => {
+  const fetchQuiz = async (url) => {
     setLoading(true);
-    fetch(url);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => {
+      console.log(err);
+    });
+
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setError(false);
+        setWaiting(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
   };
 
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex - 1;
+      if (index > questions.length - 1) {
+        openModal();
+        return 0;
+      } else {
+        return index;
+      }
+    });
+  };
+
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1);
+    }
+  };
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setCorrect(0);
+    setWaiting(true);
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setQuiz({ ...quiz, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { amount, category, difficulty } = quiz;
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+    fetchQuiz(url);
+  };
   return (
-    <AppContext.Provider value={{ quiz, error, handleChange, handleSubmit }}>
+    <AppContext.Provider
+      value={{
+        quiz,
+        error,
+        handleChange,
+        handleSubmit,
+        nextQuestion,
+        checkAnswer,
+        index,
+        loading,
+        closeModal,
+        correct,
+        waiting,
+        showModal,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
